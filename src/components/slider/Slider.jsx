6e6e2c7479c569837bc4ab6bb942b78687a42aa2
niolha <olhanikulina2@gmail.com>
2022-05-10 21:33:27 +0200

    update sliderIndex

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,36 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import ".//static/Slider.scss";
 import { ArrowLeftRounded, ArrowRightRounded } from "@mui/icons-material";
+import { sliderItems } from "../../data";
+import styled from 'styled-components';
+
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform: translateX(${props => props.slideIndex *-100}vw)
+`;
 
 const Slider = () => {
+  let [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if(direction==="left"){
+      setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2)
+    }
+    else{
+      setSlideIndex(slideIndex < 2 ? slideIndex+1 : 0)
+    }
+  }
   return (
     <div className="slider">
-      <div className="slider__arrow" id="arrow-left">
+      <div className="slider__arrow" id="arrow-left" onClick={()=>handleClick('left')}>
         <ArrowLeftRounded style={{ fontSize: 40 }} />
       </div>
-      <div className="slider__wrapper">
-        <div className="slides">
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map(item=> (
+        <div className="slides" id={item.bd}>
           <div className="slides__img-container">
             <div className="slides__img-container__image">
               <img
                 alt=""
-                src="https://res.cloudinary.com/dxlba6g7m/image/upload/v1651674438/herfst_ahz8z4.jpg"
+                src={item.img}
               />
             </div>
           </div>
           <div className="slides__inf-container">
             <div className="slides__inf-container__info">
-              <h1> BOUQUET OF THE MONTH</h1>
+              <h1> {item.title}</h1>
               <p>
-                BLOOM YOUR HOME WITH THE FLOWER OF THIS MONTH, INSPIRE YOUR
-                HOMMIES
+                {item.desc}
               </p>
               <button>SHOP NOW</button>
             </div>
           </div>
         </div>
-      </div>
-      <div className="slider__arrow" id="arrow-right">
+        ))}
+      </Wrapper>
+      <div className="slider__arrow" id="arrow-right" onClick={()=>handleClick('right')}>
         <ArrowRightRounded style={{ fontSize: 40 }} />
       </div>
     </div>
