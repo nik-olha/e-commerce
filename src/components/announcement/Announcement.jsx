e064b7317594004c9f70a72c7cdcf8dafb7701a2
niolha <olhanikulina2@gmail.com>
2022-05-10 13:42:01 +0200

    announcement

---
+++ src/components/announcement/Announcement.jsx
@@ -0,0 +1,9 @@
+import './/static/Announcement.scss'
+
+const Announcement = () => {
+  return (
+    <div className="announcement-container">Free Shipping on your first order! </div>
+  )
+}
+
+export default Announcement
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Announcement from '../components/announcement/Announcement'
 import Navbar from '../components/navbar/Navbar'
 
 const Home = () => {
   return (
     <div>
+        <Announcement/>
         <Navbar/>
     </div>
   )
